dd2dc90eb644f4e40fcba13810e2568563a52704
teja-kishore <76863392+teja-kishore@users.noreply.github.com>
2021-08-14 13:27:04 +0530

    Update Rest_api_user.py

--- Rest_api_user.py
+++ Rest_api_user.py
@@ -1,30 +1,57 @@
-from flask import Flask,request
-from flask_restful import Resource,Api
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-Data=[]
+TODOS = {
+    'todo1': {'name': 'Teja Kishore','age':21,'email':"ytkishore7@gmail.com"},
+    'todo2': {'name': 'kishore','age':21,'email':'n151127@rguktn.ac.in'},
+    'todo3': {'name': 'V.Y.T.Kishore','age':21,'email':'kishore.egnify@gmail.com'},
+}
 
-class People(Resource):
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('age')
+parser.add_argument('email')
+
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        name = {'name': args['name']}
+        age = {'age': args['age']}
+        email = {'email':args['email']}
+        TODOS[todo_id] = name
+        return name, 201
+
+class TodoList(Resource):
     def get(self):
-        for x in Data:
-            if x['Data']==__name__:
-                return x
-        return {'Data':None}
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
 
 
-    def post(self, name):
-        Tem = {'Data':name}
-        Data.append(Tem)
-        return Tem
-
-    def delete(self):
-        for ind,x in enumerate(Data):
-            if x['Data']==__name__:
-                Tem = Data.pop(ind)
-                return {'None':"Deleted"}
-
-api.add_resource(People,'/Name/<string:name>')
 if __name__ == '__main__':
     app.run(debug=True)
